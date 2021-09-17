11971155b20884dc9b8bc0d3e9a00f60e53b6648
rayudusubrahmanyam12 <rayudusubrahmanyam12@gmail.com>
2021-09-17 15:25:19 +0530

    added terraform files

---
+++ terraform/security_group.tf
@@ -0,0 +1,29 @@
+resource "aws_default_security_group" "demo-develop-default-sg" {
+   vpc_id = aws_vpc.demo-develop-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.TAGNAME}-default-sg"
+  }
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,65 @@
+resource "aws_vpc" "demo-develop-vpc" {
+  cidr_block = var.VPC-CIDR-BLOCK
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.TAGNAME}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "demo-develop-igw" {
+  vpc_id = aws_vpc.demo-develop-vpc.id
+  tags = {
+    Name = "${var.TAGNAME}-igw"
+  }
+}
+
+resource "aws_subnet" "demo-develop-subnet-1" {
+  vpc_id            = aws_vpc.demo-develop-vpc.id
+  cidr_block        = var.SUBNET-CIDR-BLOCK[0]
+  map_public_ip_on_launch = true
+  availability_zone = var.AVAIL-ZONE-1
+  tags = {
+    Name = "${var.TAGNAME}-subnet-1"
+  }
+}
+
+resource "aws_subnet" "demo-develop-subnet-2" {
+  vpc_id            = aws_vpc.demo-develop-vpc.id
+  cidr_block        = var.SUBNET-CIDR-BLOCK[1]
+  availability_zone = var.AVAIL-ZONE-2
+  tags = {
+    Name = "${var.TAGNAME}-subnet-2"
+  }
+}
+
+resource "aws_default_route_table" "demo-develop-main-rtb" {
+  default_route_table_id = aws_vpc.demo-develop-vpc.default_route_table_id
+
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.demo-develop-igw.id
+    }
+
+  tags = {
+    Name = "${var.TAGNAME}-main-rtb"
+  }
+}
+
+
+resource "aws_route_table" "demo-develop-private-rtb" {
+  vpc_id = aws_vpc.demo-develop-vpc.id
+  
+  tags = {
+    Name = "${var.TAGNAME}-private-rtb"
+  }
+}
+
+resource "aws_route_table_association" "a-route-subnet" {
+  subnet_id = aws_subnet.demo-develop-subnet-2.id
+  route_table_id = aws_route_table.demo-develop-private-rtb.id
+}
+
+
+
+
